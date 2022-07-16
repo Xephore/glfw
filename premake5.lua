febdda640e41e10afbc6af5e18f8d3f1c7aebbf1
unknown <dkerrx@gmail.com>
2022-07-16 01:57:07 -0400

    premake file fixed

--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,12 @@ project "GLFW"
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/platform.c",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
     
 	filter "system:windows"
@@ -33,7 +38,8 @@ project "GLFW"
             "src/win32_window.c",
             "src/wgl_context.c",
             "src/egl_context.c",
-            "src/osmesa_context.c"
+            "src/osmesa_context.c",
+            "src/win32_module.c"
         }
 
 		defines 
